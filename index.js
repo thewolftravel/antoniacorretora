8c23c1dcb7f99dce66dd2966f305839883148963
thewolftravel <bittencourtkah@gmail.com>
2023-11-03 18:05:35 -0500

    added image

---
+++ index.js
@@ -0,0 +1,15 @@
+console.log("Hello World");
+window.addEventListener("scroll", setScrollVar);
+window.addEventListener("resize", setScrollVar);
+
+function setScrollVar() {
+  const htmlElement = document.documentElement;
+  const percentOfScreenHeightScrolled =
+    htmlElement.scrollTop / htmlElement.clientHeight;
+  htmlElement.style.setProperty(
+    "--scroll",
+    Math.min(percentOfScreenHeightScrolled * 100, 100)
+  );
+}
+
+setScrollVar();
